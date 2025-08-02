fa36b396c192a03956ba830047313e81acac78ec
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-08-02 19:01:54 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
-        # Use a managed database from the DATABASE_URL environment variable
-        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+        # Correctly format the database URL for SQLAlchemy
+        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
@@ -30,16 +30,6 @@ def create_app(test_config=None):
     bcrypt.init_app(app)
     csrf.init_app(app)
     db.init_app(app)  # Initialize Flask-SQLAlchemy with the app
-
-    with app.app_context():
-        # A simple method to ensure tables are created on startup
-        db.create_all()
-        # Add the admin user if it doesn't exist
-        if not User.query.filter_by(username='admin').first():
-            admin_user = User(username='admin', role='admin')
-            admin_user.set_password('adminpassword')
-            db.session.add(admin_user)
-            db.session.commit()
 
     from .routes import main_bp
     app.register_blueprint(main_bp)
