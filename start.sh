4b177037f6c716915b93d9d17e467fb60ab54974
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-08-02 14:51:04 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# This script is for starting the Gunicorn server
-# It's important to use the app:create_app() syntax because your app is created with a factory function.
-# The number of workers (-w) can be adjusted based on your needs.
+# Create the instance directory if it doesn't exist
+mkdir -p instance
+
+# Run the database initialization script
+python init_db_and_user.py
+
+# Start the Gunicorn server
 gunicorn --bind 0.0.0.0:10000 --workers 4 'app:create_app()'
